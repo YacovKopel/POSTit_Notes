44d564a35eafc0df51d75823a2baeeb9d2d697a3
Yacov Kopel <kokokopel94@gmail.com>
2023-01-11 09:44:30 -0800

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
